2482838c42c861c21c7ce06a7b572d60abfdd396
Corbin Klett <corbinklett@gmail.com>
2013-06-29 15:33:42 -0400

    initial web app upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ajoslin.mobile-navigate','ngMobile', 'ngUpload'])
+    .config(function ($compileProvider){
+        $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    })
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+        $routeProvider.otherwise({redirectTo: '/'});
+  }]);
